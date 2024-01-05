502e8f6f8f419a85c1e715f643fab72f3dc494be
RASBI <74168235+RASBI-CARDNAME@users.noreply.github.com>
2024-01-05 23:01:43 +0900

    V1.2.0 업데이트
    EfsTools 커맨드 입력 지원
    QPST / PDC 커맨드 명령어 재수정(setprop sys.usb.config diag,serial_cdev,rmnet,adb)
    boot.img 설치 버그 수정
    부트로더 언락 설명 문구 수정
    코드 최적화

--- Sony_VOLTE_Helper.py
+++ Sony_VOLTE_Helper.py
@@ -1,3 +1,5 @@
+#Made By RASBI
+
 #Library
 import os
 import subprocess
@@ -7,7 +9,9 @@ from tkinter import filedialog
 #variable
 number=''  # menu number variable
 KEY='' #key for bootloader unlock
-file_path =None #adress for flashing boot.img
+file_path = None #address for flashing boot.img
+folder_path = None  #address for EFSTOOLS DIR
+VERSION = '1.2.0'
 
 #함수
 def Clear_page():
@@ -18,33 +22,41 @@ def Clear_page():
 def Make_line():
     print("--------------------")
 
+def Re_select():
+    Make_line()
+    print("다시 선택해주세요.")
+    Make_line()
+
 def Main_menu():
+    Make_line()
     print("1. 리커버리로 재부팅")
     print("2. Fastboot로 재부팅")
     print("3. Fastboot 탈출(#os로 부팅)")
     print("4. 부트로더 언락(#주의)")
     print("5. boot.img 플래시(#주의)")
     print("6. init_boot.img 플래시(#주의)")
-    print("7. EFSTOOL 포트 개방")
+    print("7. EfsTools 작업")
     print("8. PDC / QPST 포트 열기")
     print("9. VOLTE 스위치 열기")
     print("10. ADB / FASTBOOT 사용(명령어 직접 입력)")
     print("11. 기기 재부팅")
     print("12. 기타")
     print("13. 종료")
+    Make_line()
 
 def open_file_dialog():
     while True:
-        Confirm_num = 0;
+        Confirm_num = 0; #확인 용 변수
         global file_path #전역 변수로 사용할 거라고 알림.
         
-        root = tk.Tk()
+        root = tk.Tk() 
         root.withdraw()  # 기본 창 숨기기
 
         file_path = filedialog.askopenfilename()  # 파일 선택 창 열기
 
         if file_path:
             Clear_page()
+            
             Make_line()
             print("선택한 파일의 경로:", file_path,"\n")
             Make_line()
@@ -52,26 +64,22 @@ def open_file_dialog():
             Make_line()
             
             Confirm_num=input()
-            #print(Confirm_num)
 
             if Confirm_num == '0' :#다시 선택
                 Clear_page()
-                Make_line()
-                print("다시 선택해주세요.")
-                Make_line()
+                Re_select()
                 continue
 
             elif Confirm_num == '1': #확인
                 print("")
+                break
 
             else:
                 Clear_page()
-                Make_line()
-                print("다시 선택해주세요.")
-                Make_line()
+                Re_select()
                 continue
             
-        else:
+        else: # 파일 미선택 시
             Clear_page()
             Make_line()
             print("파일을 선택하지 않았습니다. 처음으로 돌아갑니다.")
@@ -79,27 +87,69 @@ def open_file_dialog():
             print("\n")
             break
 
-# 경고문
-print("-----Sony VOLTE Helper------")
-print('1. FASTBOOT & ADB 드라이버가 설치되어 있어야 합니다')
-print('2. 이외 각종 드라이버를 설치한 상태로 사용해주세요.')
-print("3. USB 디버그 모드와 oem 잠금 해제를 활성화 해주세요.")
-print('4. 이 프로그램은 ADB / FASTBOOT 명령어를 간단하게 사용할 수 있게 해줍니다.')
-print('5. 사용시 일어나는 문제에 대한 책임은 사용자에게 있습니다.')
-Make_line()
-print('\n')
-input('모두 읽었고 동의하신다면 Enter를 눌러주세요...')
+def get_folder_path():
+    while True:
+        Confirm_num = 0; #확인 용 변수
+        global folder_path  # 함수 내에서 전역 변수를 사용하겠다고 선언
 
-Clear_page()
+        root = tk.Tk()
+        root.withdraw()  # 기본 창 숨기기
+
+        folder_path = filedialog.askdirectory()  # 폴더 선택 창 열기
+
+        if folder_path:
+            Clear_page()
+            
+            Make_line()
+            print("선택한 EFSTOOLS 폴더의 경로:", folder_path,"\n")
+            Make_line()
+            print ("맞으면 1 입력 후 엔터, 다시 선택하려면 0 입력 후 엔터")
+            Make_line()
+            
+            Confirm_num=input()
+
+            if Confirm_num == '0' :#다시 선택
+                Clear_page()
+                Re_select()
+                continue
+
+            elif Confirm_num == '1': #확인
+                print("")
+                break
+
+            else:
+                Clear_page()
+                Re_select()
+                continue
+            
+        else:
+            Clear_page()
+            Make_line()
+            print("폴더를 선택하지 않았습니다. 처음으로 돌아갑니다.")
+            Make_line()
+            print("\n")
+            break
+
+def warning() : # 경고문
+    print("-----Sony VOLTE Helper",VERSION,"------")
+    print('1. FASTBOOT & ADB 드라이버가 설치되어 있어야 합니다')
+    print('2. 이외 각종 드라이버를 설치한 상태로 사용해주세요.')
+    print("3. USB 디버그 모드와 oem 잠금 해제를 활성화 해주세요.")
+    print('4. 이 프로그램은 ADB / FASTBOOT 명령어를 간단하게 사용할 수 있게 해줍니다.')
+    print('5. 사용시 일어나는 문제에 대한 책임은 사용자에게 있습니다.')
+    Make_line()
+    print('\n')
+    input('모두 읽었고 동의하신다면 Enter를 눌러주세요...')
+    Clear_page()
+
+warning()
 
 # 메인 코드 구동 부
 while True:
     file_path =None #에러 방지용 변수 초기화
     
-    print("-----Sony VOLTE Helper------")
-    Make_line()
+    print("-----Sony VOLTE Helper",VERSION,"------")
     Main_menu()
-    Make_line()
     number = input("번호 입력 => ")
 
     if number == '1': #reboot recovery
@@ -154,10 +204,20 @@ while True:
         Clear_page()
         print("입력하신 키:"+KEY)
         input('확인 후 Enter를 눌러주세요...')
-        
-        ## 확인 2차
+
+        os.system('adb devices')
+        os.system('adb reboot bootloader')
+
         Clear_page()
-        input('Fastboot MODE 진입 후, 장치관리자 확인 후 Enter를 눌러주세요...')
+        Make_line()
+        print("장치관리자에서 드라이버 확인 후 엔터를 눌러주세요...")
+        Make_line()
+        input()
+
+        Clear_page()
+        Make_line()
+        print("오래 걸리니 인내심을 갖고 기다려 주세요...")
+        Make_line()
         
         ##언락 수행
         os.system(f'fastboot oem unlock 0x{KEY}')
@@ -260,33 +320,68 @@ while True:
         Make_line()
         print("\n")
         
-    elif number == '7' : #efstool 사용용 포트 개방
+    elif number == '7' : #efstool 사용
         Clear_page()
         Make_line()
         print("##경고##")
         Make_line()
         print("1. Magisk 루팅 후 사용해주세요.")
         print("2. 스마트폰에 슈퍼유저 권한 요청이 나오면 허용해주세요.")
-        print("3. EFStool 포트 개방만 해주는 명령어 입니다.")
-        print("4. 수행하는 명령어는 \"setprop persist.usb.eng 1\" 입니다.")
-        Make_line()
-        print("EfsTools.exe writeFile -i mcfg_autoselect_by_uim -o ~~~ 작업은 직접 해주셔야 합니다!!")
+        print("3. EFS 파일 넣는 작업(QPST)은 따로 해주셔야 합니다.")
+        print("4. 수행하는 명령어는 다음과 같습니다:\n")
+        print("setprop persist.usb.eng 1")
+        print("EfsTools.exe writeFile -i mcfg_autoselect_by_uim -o /nv/item_files/mcfg/mcfg_autoselect_by_uim")
+        print("EfsTools.exe writeFile -i mcfg_autoselect_by_uim -o /nv/item_files/mcfg/mcfg_autoselect_by_uim_Subscription01")
+        print("EfsTools.exe writeFile -i mcfg_autoselect_by_uim -o /nv/item_files/mcfg/mcfg_autoselect_by_uim_Subscription02\n")
         Make_line()
         print('\n')
         input('모두 읽었다면 Enter를 눌러주세요...')
 
         Clear_page()
 
-        ##작업 수행 부
+        ##작업 수행 부 1
         command = ['adb', 'shell']
         input_command = 'su\nsetprop persist.usb.eng 1\n'
 
         result = subprocess.run(command, text=True, input=input_command, capture_output=True, encoding='utf-8')
 
         Clear_page()
+        Make_line()
+        print("1. 장치관리자에서 확인해주시고 기기 안잡히면 USB 재연결 or VOLTE 강좌 참고 해주세요.")
+        print("2. 그 다음 퀄컴 드라이버 작업 후 엔터를 눌러주세요...")
+        Make_line()
+        input()
+
+        Clear_page()
+        Make_line()
+        print("포트 에러시 아래 링크 참조:")
+        print("https://cafe.naver.com/x1smart/590963")
+        print("\n모두 읽었다면 Enter를 눌러주세요...")
+        Make_line()
+        input()
+
+        Clear_page()
+        Make_line()
+        print("새로 열린 창에서 EfsTools 폴더를 선택해주세요...")
+        Make_line()
+        
+        ##작업 수행 부 2
+        get_folder_path()
+
+        if folder_path == '' :
+            continue
+
+        current_directory = os.getcwd()
+        os.chdir(f'"{folder_path}"')
+        os.system('EfsTools.exe writeFile -i mcfg_autoselect_by_uim -o /nv/item_files/mcfg/mcfg_autoselect_by_uim')
+        os.system('EfsTools.exe writeFile -i mcfg_autoselect_by_uim -o /nv/item_files/mcfg/mcfg_autoselect_by_uim_Subscription01')
+        os.system('EfsTools.exe writeFile -i mcfg_autoselect_by_uim -o /nv/item_files/mcfg/mcfg_autoselect_by_uim_Subscription02')
+        os.chdir(f'"{current_directory}"')
+        
+        Clear_page()
             
         Make_line()
-        print("포트 개방 완료.")
+        print("작업 완료.")
         Make_line()
         print("\n")
 
@@ -298,7 +393,8 @@ while True:
         print("1. Magisk 루팅 후 사용해주세요.")
         print("2. 스마트폰에 슈퍼유저 권한 요청이 나오면 허용해주세요.")
         print("3. PDC / QPST 포트 개방을 해주는 명령어 입니다.")
-        print("4. 수행하는 명령어는 \"setprop sys.usb.config diag, serial_cdev, rmnet, adb\" 입니다.")
+        print("4. 수행하는 명령어는 다음과 같습니다:")
+        print("setprop sys.usb.config diag,serial_cdev,rmnet,adb")
         Make_line()
         print('\n')
         input('모두 읽었다면 Enter를 눌러주세요...')
@@ -366,11 +462,11 @@ while True:
         Clear_page()
         Make_line()
         print("Made by RASBI")
-        print("최종 업데이트: 2024-01-04\n")
-        print("V 1.1.1")
-        print("업데이트 내용: 재부팅 기능 및 boot.img 플래시 기능 추가\n")
+        print("최종 업데이트: 2024-01-05\n")
+        print("V 1.2.0")
+        print("업데이트 내용: EFS 툴 사용 기능 추가 / QPST PDC 명령어 수정\n")
         Make_line()
-        print("VOLTE 강좌를 제공해 주신 소니 사용자 모임 카페의 앨리자님 및 회원분들께 감사의 말씀드립니다.")
+        print("VOLTE 강좌를 제공해 주신 소니 사용자 모임 카페의 앨리자님과 소스케님, 회원분들께 감사의 말씀드립니다.")
         Make_line()
         input("모두 읽었다면 Enter를 눌러주세요...\n--------------------")
         Clear_page()
